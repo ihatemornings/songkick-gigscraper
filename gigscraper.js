b32550c0dc2530682faa7cd1044ce9f0be54ce77
Ben Walker <ben@wallpaperjazz.com>
2010-09-20 16:15:51 +0100

    Displays ticket link and formats listings as basic hEvent

--- gigscraper.js
+++ gigscraper.js
@@ -3,7 +3,7 @@
 	$.fn.gigscraper = function(myspace_id, options) {
 		
 		settings = jQuery.extend({
-			php_url: "http://secondhead.co.uk/gigscraper/gigscraper.php"
+			php_url: "http://secondhead.co.uk/gigscraper/v3/gigscraper.php"
 		}, options);
 		
 		return this.each(function() {
@@ -14,7 +14,16 @@
 			$.getJSON(settings.php_url + "?format=json&id=" + myspace_id + "&callback=?", function(data) {
 				$o = $("<table><tbody></tbody></table>");
 				for(var i in data) {
-					$o.append('<tr><th>' + data[i].date + '</th><td>' + data[i].info + '</td></tr>');
+					var infobits = data[i].info.split(", ");
+					var summary = infobits[0];
+					var location = infobits[1];
+					if (infobits.length > 2) {
+						if (infobits[2] != infobits[1] && !/^\s+$/.test(infobits[2])) {
+							location = location + ', ' + infobits[2];
+						}
+					}
+					var ticketlink = (data[i].ticketlink) ? ', <a href="' + data[i].ticketlink + '">buy tickets</a>' : '';
+					$o.append('<tr class="vevent"><th>' + data[i].date + '</th><td><span class="summary">' + summary + '</span>, <span class="location">' + location + '</span><br /><span class="meta">' + data[i].time + ' start' + ticketlink + '</span></td></tr>');
 				}
 				$listing.empty().append($o);
 			});
