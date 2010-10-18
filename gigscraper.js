1dd4f3eccb5493aad69bccbc40be8c5e3401c144
Ben Walker <ben@wallpaperjazz.com>
2010-10-18 14:24:20 +0100

    $.gigscraper() now filters out past gigs

--- gigscraper.js
+++ gigscraper.js
@@ -9,21 +9,24 @@
 		return this.each(function() {
 		
 			var $listing = $(this);
+			var now = new Date();
 			
 			$listing.append("<p>Loading gigs...</p>");
 			$.getJSON(settings.php_url + "?format=json&id=" + myspace_id + "&callback=?", function(data) {
 				$o = $("<table><tbody></tbody></table>");
 				for(var i in data) {
-					var infobits = data[i].info.split(", ");
-					var summary = infobits[0];
-					var location = infobits[1];
-					if (infobits.length > 2) {
-						if (infobits[2] != infobits[1] && !/^\s+$/.test(infobits[2])) {
-							location = location + ', ' + infobits[2];
+					if ((data[i].timestamp * 1000) > now.getTime()) {
+						var infobits = data[i].info.split(", ");
+						var summary = infobits[0];
+						var location = infobits[1];
+						if (infobits.length > 2) {
+							if (infobits[2] != infobits[1] && !/^\s+$/.test(infobits[2])) {
+								location = location + ', ' + infobits[2];
+							}
 						}
+						var ticketlink = (data[i].ticketlink) ? ', <a href="' + data[i].ticketlink + '">buy tickets</a>' : '';
+						$o.append('<tr class="vevent"><th>' + data[i].date + '</th><td><span class="summary">' + summary + '</span>, <span class="location">' + location + '</span><br /><span class="meta"><abbr class="dtstart" title="' + data[i].dtstart + '">' + data[i].time + '</abbr> <abbr class="dtend" title="' + data[i].dtend + '">start</abbr>' + ticketlink + '</span></td></tr>');
 					}
-					var ticketlink = (data[i].ticketlink) ? ', <a href="' + data[i].ticketlink + '">buy tickets</a>' : '';
-					$o.append('<tr class="vevent"><th>' + data[i].date + '</th><td><span class="summary">' + summary + '</span>, <span class="location">' + location + '</span><br /><span class="meta"><abbr class="dtstart" title="' + data[i].dtstart + '">' + data[i].time + '</abbr> <abbr class="dtend" title="' + data[i].dtend + '">start</abbr>' + ticketlink + '</span></td></tr>');
 				}
 				$listing.empty().append($o);
 			});
