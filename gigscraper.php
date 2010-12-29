a739abd0b625f702428f2920aaf6a5f554ae513a
Ben Walker <ben@ihatemornings.com>
2010-12-29 14:14:06 +0000

    Revert "Modified to scrape and cache all pages of the Myspace event listings."
    This reverts commit 454118fa98a6d61796766bb972485e22f0a4f0bd.
    
    Conflicts:
    
    	gigscraper.php

--- gigscraper.php
+++ gigscraper.php
@@ -91,60 +91,51 @@ function scrape($myspace_id) {
 	
 	global $debugging; if ($debugging) echo "<p>Scraping Myspace...</p>";
 	
+	$myspace_url = "http://events.myspace.com/$myspace_id/Events/1";
+	
+	// TODO: scrape more than one page
+	
 	ini_set('user_agent', 'Scrape/2.5');
-	$pageCount = 0;
-	
-	do {
-		$gotResults = false;
-		$pageCount++;
-		$html = file_get_html("http://events.myspace.com/$myspace_id/Events/$pageCount");
+	$html = file_get_html($myspace_url);
 
-		// Use simplehtmldom to scrape gig listings into an array
-		$showsArray = $html->find('#home-rec-events .eventitem');
-		if (count($showsArray) > 0) {
-			$gotResults = true;
-			foreach ($showsArray as $v) {
-
-				$item['title'] = $v->find('a.event-title span',0)->plaintext;
-
-				$item['info'] = $v->find('div.event-titleinfo span span',0)->plaintext;
-
-				$item['ticketlink'] = $v->find('a.ticketFindLink',0)->href;
-
-				$rawdate = $v->find('div.event-cal',0)->plaintext;
-
-				if (preg_match("/(.*), (.*) @ (.*)/i", $rawdate, $datebits)) {
-
-					$timestamp = strtotime($datebits[2] . $datebits[3]);
-					$item['time'] = $datebits[3];
-
-				} elseif (preg_match("/(.*) @ (.*)/i", $rawdate, $datebits)) {
-
-					// When date is 'Today' or 'Tomorrow'
-					$timestamp = strtotime($datebits[1] . $datebits[2]);
-					$item['time'] = $datebits[2];
-
-				}
-				if ($timestamp) {
-					$item['timestamp'] = $timestamp;
-					$item['dtstart'] = date(DATE_ISO8601, $timestamp);
-					$item['dtend'] = date(DATE_ISO8601, $timestamp + 10800);
-					$item['date'] = date("D d M", $timestamp);
-				} else {
-					$item['timestamp'] = '';
-					$item['dtstart'] = '';
-					$item['dtend'] = '';
-					$item['date'] = '';
-					$item['time'] = '';
-				}
-
-				$shows[] = $item;
-			}
+	// Use simplehtmldom to scrape gig listings into an array
+	foreach ($html->find('#home-rec-events .eventitem') as $v) {
+		
+		$item['title'] = $v->find('a.event-title span',0)->plaintext;
+		
+		$item['info'] = $v->find('div.event-titleinfo span span',0)->plaintext;
+		
+		$item['ticketlink'] = $v->find('a.ticketFindLink',0)->href;
+		
+		$rawdate = $v->find('div.event-cal',0)->plaintext;
+		
+		if (preg_match("/(.*), (.*) @ (.*)/i", $rawdate, $datebits)) {
+			
+			$timestamp = strtotime($datebits[2] . $datebits[3]);
+			$item['time'] = $datebits[3];
+			
+		} elseif (preg_match("/(.*) @ (.*)/i", $rawdate, $datebits)) {
+			
+			// When date is 'Today' or 'Tomorrow'
+			$timestamp = strtotime($datebits[1] . $datebits[2]);
+			$item['time'] = $datebits[2];
+			
 		}
+		if ($timestamp) {
+			$item['timestamp'] = $timestamp;
+			$item['dtstart'] = date(DATE_ISO8601, $timestamp);
+			$item['dtend'] = date(DATE_ISO8601, $timestamp + 10800);
+			$item['date'] = date("D d M", $timestamp);
+		} else {
+			$item['timestamp'] = '';
+			$item['dtstart'] = '';
+			$item['dtend'] = '';
+			$item['date'] = '';
+			$item['time'] = '';
+		}
+		
+		$shows[] = $item;
 	}
-	while ($gotResults);
-	
-	return $shows;
 }
 
 function display_shows($shows, $format = "html") {
